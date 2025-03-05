2c98451b1c95d4ddf38ad8ca0ba1433f5f897314
gulnurkaztai <93098410+gulnurkaztai@users.noreply.github.com>
2025-03-05 16:57:49 -0500

    day 28

---
+++ day28/Solution.java
@@ -0,0 +1,25 @@
+package day28;
+
+public class Solution {
+    public boolean isPalindrome(String s) {
+        int left =0;
+        int right =s.length()-1;
+        s = s.toLowerCase();
+        while(left < right){
+            if(!Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
+            else if(!Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            } else if (s.charAt(left) != s.charAt(right)){
+                return false;
+            } else {
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+}{
+
+}
